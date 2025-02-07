c22af6e5f72aadc324b293c75d51f35c3418c1e9
ShreyasN <shreyasnaik81@gmail.com>
2025-02-07 20:47:06 +0530

    modified schemas for customform and options

--- Models/QuestionsSchema.js
+++ Models/QuestionsSchema.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
-    formId: { type: mongoose.Schema.Types.ObjectId, ref: "Form" },
+    formId: {},
     text: String,
-    options: [
-        String
-    ]
-});
+    options:
+     {
+        option1:{
+            type:String,
+            required:true,
+        },option2:{
+            type:String,
+            required:true,
+        },option3:{
+            type:String,
+            required:true,
+        },option4:{
+            type:String,
+            required:true,
+        }
+    }
+}
+);
 
 
 const Question = mongoose.models.Question || mongoose.model('Question', questionSchema);
